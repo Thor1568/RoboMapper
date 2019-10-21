bc7dbcbd1065a5f207b306d5d0f6f637f2f72d72
Thor1568 <n0002k2@gmail.com>
2019-10-20 22:54:33 -0400

    deejMapper python library made

--- schematics/SubProjects/Mapping/DeejMapper.py
+++ schematics/SubProjects/Mapping/DeejMapper.py
@@ -4,10 +4,25 @@ Unlimited mode and Limited mode.
 Unlimited is outdoors where there are not obstacles everywhere and the
 robot will patrol a clearly defined area.
 Limited is indoors where there are obstacles and it will patrol until it
-is disabled?
-
+is disabled? Maybe merge them but here are framework classes for how it will work
 
 """
+class Robot():
+    """
+    'Robot' that acts as a virtual placeholder for the actual thing.
+    could be visualized some day or something
+    """
+    def __init__(self, scale):
+        self.x = 0; self.y = 0;
+
+
+    def move(self, dx, dy):
+        #move deltaX and deltaY or change in x and y
+        self.x += dx*scale
+        self.y += dy*scale
+
+    def _reset(self):
+        self.x = 0; self.y = 0;
 
 
 class UnlimMap():
@@ -21,7 +36,7 @@ class UnlimMap():
         self.x_dim = x_dim; self.y_dim = y_dim;
         self.scale = scale
         self.map = []
-        self.createMap(x_dim, y_dim)
+        self._createMap(x_dim, y_dim)
 
 
     def addBarrier(self, x, y):
@@ -30,36 +45,47 @@ class UnlimMap():
     def removeBarrier(self, x, y):
         self.map[y][x] = 0
 
-    def createMap(self, x_dim, y_dim):
+    def _createMap(self, x_dim, y_dim):
         for x in range(y_dim):
             #Creates a row
             self.map.append([])
             for p in range(x_dim):
                 self.map[x].append(0)
-        print(self.map)
 
     def saveMap(self, file):
         f = open(file, "w")
         for y in range(len(self.map)):
-            for x in range(y):
-                f.write(str(self.map[y][x]))
-                f.write(" ")
-            f.write("\n")
+            f.writelines(str(self.map[y])+"\n")
 
 class LimMap():
     def __init__(self, scale, max_size):
         self.max = max_size
         self.scale = scale
 
+    def addBarrier(self, x, y):
+        self.map[y][x] = 1
+
+    def removeBarrier(self, x, y):
+        self.map[y][x] = 0
+
+    def _createMap(self):
+        pass
+        #? how to go about this?
+        #robot needs to FIND dimensions i guess
+
+    def saveMap(self, file):
+        f = open(file, "w")
+        for y in range(len(self.map)):
+            f.writelines(str(self.map[y])+"\n")
 
 
 def main():
+    import os
     amap = UnlimMap(5, 2, 20)
     amap.addBarrier(0, 0)
     amap.addBarrier(4, 1)
-    print(amap.map)
-    print(amap.map[0])
-    amap.saveMap("testmap.txt")
+    #pls fix this, it saves elsewhere thanks Python very cool
+    #amap.saveMap("testerMap.txt")
 
 
 if __name__ == '__main__':
