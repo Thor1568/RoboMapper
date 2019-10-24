f7952f4258d0aae85010496ed2f32ce15a03f861
Thor1568 <n0002k2@gmail.com>
2019-10-23 21:56:57 -0400

    merge visualizer and mapper file

--- schematics/SubProjects/Mapping/DeejMapper.py
+++ schematics/SubProjects/Mapping/DeejMapper.py
@@ -7,6 +7,7 @@ Limited is indoors where there are obstacles and it will patrol until it
 is disabled? Maybe merge them but here are framework classes for how it will work
 
 """
+
 class Robot():
     """
     'Robot' that acts as a virtual placeholder for the actual thing.
@@ -14,15 +15,33 @@ class Robot():
     """
     def __init__(self, scale):
         self.x = 0; self.y = 0;
+        self.deltaX = 0
+        self.dist = 0
 
-
-    def move(self, dx, dy):
+    def moveRaw(self, dx, dy):
         #move deltaX and deltaY or change in x and y
         self.x += dx*scale
         self.y += dy*scale
+        self.deltaX += dx*scale+dy*scale
+        self.dist += abs(dx*scale)+abs(dy*scale)
+
+    def move(self, dx, dy, map):
+        #moves with check
+        check = canMove(dx, dy, map)
+        if check:
+            moveRaw(dx, dy)
+
+    def canMove(self, dx, dy, map):
+        future = (self.x+dx, self.y+dy)
+        newTile = map[future[1]][future[0]]
+        if newTile == (0 or 2):
+            return True
+
 
     def _reset(self):
         self.x = 0; self.y = 0;
+        self.deltaX = 0
+        self.dist = 0
 
 
 class UnlimMap():
@@ -79,6 +98,45 @@ class LimMap():
             f.writelines(str(self.map[y])+"\n")
 
 
+
+class MapObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, otype):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect = self.image.get_rect()
+        self.place = (x, y)
+        self.rect.x = x
+        self.rect.y = y
+        self.objType = otype
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+        self.place[0] += dx
+        self.place[1] += dy
+
+
+class VisualMap(pygame.Surface):
+    def __init__(self, width, height):
+        pygame.Surface.__init__(self, size=(width, height))
+        self.pos = (width, height)
+        self.RECT = (0, 0, width, height)
+        self.INRECT = (edge,edge, width-(edge), height-(edge))
+        pygame.draw.rect(self, WHITE, self.RECT)
+        pygame.draw.rect(self, BLACK, self.INRECT)
+        self.objects = pygame.sprite.Group()
+
+    def genBarriers(self, map):
+        pass
+        #reads barrier count and then adds them as sprites in objects
+
+    def createRobot(self, scale, map):
+        self.worldMap = map
+        self.robot = Robot(scale, map)
+
+    def render(self, display):
+        "Draws surface on the display"
+        display.blit(self, (0, self.pos[1]))
+
 def main():
     import os
     amap = UnlimMap(5, 2, 20)
@@ -86,6 +144,13 @@ def main():
     amap.addBarrier(4, 1)
     #pls fix this, it saves elsewhere thanks Python very cool
     #amap.saveMap("testerMap.txt")
+
+    #Example
+
+    #scale in centimeters?
+    scale = 50
+    bot = Robot(scale)
+    botMap = UnlimMap(10, 10, scale)
 
 
 if __name__ == '__main__':
