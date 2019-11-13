b6fde123e5b87ec1a49fb4591c5b630e045b0433
Thor1568 <n0002k2@gmail.com>
2019-11-12 21:24:59 -0500

    updates

--- schematics/SubProjects/Radio/Controller/Controller.ino
+++ schematics/SubProjects/Radio/Controller/Controller.ino
@@ -5,6 +5,7 @@ from joystick or use keypad
 
 
 */
+#include <LiquidCrystal.h>
 
 //joystick
 #define joyXPin A1
@@ -12,11 +13,17 @@ from joystick or use keypad
 #define joyBPin 2
 int joyX, joyY, joyB;
 
+const int d7 = 6, d6 = 7, d5 = 8, d4 = 9, en = 10, rs = 11;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(joyXPin, INPUT);
   pinMode(joyYPin, INPUT);
   pinMode(joyBPin, INPUT);
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("21");
 }
 
 
--- schematics/SubProjects/Radio/Controller/d_mainloop.ino
+++ schematics/SubProjects/Radio/Controller/d_mainloop.ino
@@ -4,5 +4,8 @@ void loop() {
   joyY = analogRead(joyYPin);
   joyB = digitalRead(joyBPin);
 
-  
+  lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.print("OwO");
+  delay(500);
 }
