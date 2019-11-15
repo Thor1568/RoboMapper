ab82660b86e78a2e08cf680310385ef246681e21
Thor1568 <n0002k2@gmail.com>
2019-11-15 00:54:17 -0500

    more updates OwO

--- schematics/motors/transistorControlMotor/transistorControlMotor.ino
+++ schematics/motors/transistorControlMotor/transistorControlMotor.ino
@@ -1,6 +1,6 @@
 //transisitor stuff
 
-const int TsPin = 11;
+const int TsPin = 13;
 int motorPower = 200;
 const int TsReadPin = 12;
 
@@ -8,14 +8,18 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Starting owo");
+  pinMode(TsPin, OUTPUT);
+  pinMode(TsReadPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(TsPin, HIGH);
+  Serial.println("Ts Pin is high");
   Serial.println(digitalRead(TsReadPin));
-  digitalWrite(TsPin, LOW);
   delay(1000);
+  digitalWrite(TsPin, LOW);
+  Serial.println("Ts Pin is low");
   Serial.println(digitalRead(TsReadPin));
   delay(1000);
 }
